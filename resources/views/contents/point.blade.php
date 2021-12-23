c7c37be3acbf11522f9683312f9d77de180cafaa
Jordan-18 <jordanistiqlal@unida.gontor.ac.id>
2021-12-23 14:25:29 +0700

    ganti bentuk card tabel

--- resources/views/contents/point.blade.php
+++ resources/views/contents/point.blade.php
@@ -11,55 +11,67 @@
           <button href="#" class="btn btn-sm btn-success shadow-sm" type="submit" data-toggle="modal" data-target="#staticBackdrop">
               &#10010; Add New</button>
       </div>
-      {{-- start --}}
-      <table class="table table-bordered text-center">
-          <thead>
-            <tr>
-              <th>NAMA</th>
-              <th>Jarak</th>
-              <th>Tanggal</th>
-              <th>Jenis Busur</th>
-              <th>Rambahan</th>
-            </tr>
-            <tr>
-                <th></th>
-                <th>
-                  <a type="submit" data-toggle="modal" data-target="#jarak">
-                      &#10010; Add</a>
-                </th>
-                 <th id="date">{{($date)}}</th>
-                <th>
-                  <a type="submit" data-toggle="modal" data-target="#jenis">
-                      &#10010; Add</a>
-                </th>
-                <th></th>
-            </tr>
-          </thead>
-          <tbody class="table table-bordered text-center">
-            {{-- Start Rambahan 1 --}}
-            <tr>
-              <th rowspan="2">1</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td rowspan="2">
-                <button class="btn btn-primary" type="submit" data-toggle="modal" data-target="#staticBackdrop">Get Point</button>
-              </td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            {{-- End Rambahan 1 --}}
-            {{-- Start Rambahan 1 --}}
-            <tr>
-              <th colspan="4">Jumlah</th>
-              <td></td>
-            </tr>
-            {{-- End Rambahan 1 --}}
-          </tbody>
-        </table>
-      {{-- end --}}
+              <!-- Start Table -->
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Point</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        </div>
+                          {{-- start --}}
+                          <table class="table table-bordered text-center">
+                            <thead>
+                              <tr>
+                                <th>NAMA</th>
+                                <th>Jarak</th>
+                                <th>Tanggal</th>
+                                <th>Jenis Busur</th>
+                                <th>Rambahan</th>
+                              </tr>
+                              <tr>
+                                  <th></th>
+                                  <th>
+                                    <a type="submit" data-toggle="modal" data-target="#jarak">
+                                        &#10010; Add</a>
+                                  </th>
+                                  <th id="date">{{($date)}}</th>
+                                  <th>
+                                    <a type="submit" data-toggle="modal" data-target="#jenis">
+                                        &#10010; Add</a>
+                                  </th>
+                                  <th></th>
+                              </tr>
+                            </thead>
+                            <tbody class="table table-bordered text-center">
+                              {{-- Start Rambahan 1 --}}
+                              <tr>
+                                <th rowspan="2">1</th>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td rowspan="2">
+                                  <button class="btn btn-primary" type="submit" data-toggle="modal" data-target="#staticBackdrop">Get Point</button>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                              </tr>
+                              {{-- End Rambahan 1 --}}
+                              {{-- Start Rambahan 1 --}}
+                              <tr>
+                                <th colspan="4">Jumlah</th>
+                                <td></td>
+                              </tr>
+                              {{-- End Rambahan 1 --}}
+                            </tbody>
+                          </table>
+                        {{-- end --}}
+                      </div> 
+                </div>
+            </div>
   </div>
 @endsection
